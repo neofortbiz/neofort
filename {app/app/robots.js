3a44698c285bb3cdbfb29f1159ba1766ecd06fb9
Neofort <dan.calutu@gmail.com>
2026-03-09 15:17:46 +0200

    fix: contact page - no external imports

--- {app/app/robots.js
+++
@@ -1,7 +0,0 @@
-export default function robots() {
-  return {
-    rules: [{ userAgent: '*', allow: '/', disallow: '/api/' }],
-    sitemap: 'https://www.neofort-biz.ro/sitemap.xml',
-    host: 'https://www.neofort-biz.ro',
-  };
-}
--- {app/i18n/routing.js
+++
@@ -1,5 +0,0 @@
-import {defineRouting} from 'next-intl/routing';
-export const routing = defineRouting({
-  locales: ['ro', 'en', 'de', 'fr', 'es', 'it'],
-  defaultLocale: 'ro'
-});
